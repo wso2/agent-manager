745abc40a55e0f887291c100940717e5dbff972e
Rasika Maduranga <rasikat77@gmail.com>
2026-01-21 10:38:42 +0530

    Remove custom MUI theme export and related files

--- console/workspaces/libs/views/src/component/MainLayout/MainLayout.test.tsx
+++ console/workspaces/libs/views/src/component/MainLayout/MainLayout.test.tsx
@@ -17,14 +17,13 @@
  */
 
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { ThemeProvider, CssBaseline } from '@wso2/oxygen-ui';
+import { ThemeProvider, CssBaseline, OxygenTheme } from '@wso2/oxygen-ui';
 import { describe, it, expect, vi } from 'vitest';
 import { MainLayout } from './MainLayout';
-import { aiAgentTheme } from '../../theme';
 
 const renderWithTheme = (component: React.ReactElement) => {
   return render(
-    <ThemeProvider theme={aiAgentTheme}>
+    <ThemeProvider theme={OxygenTheme}>
       <CssBaseline />
       {component}
     </ThemeProvider>
