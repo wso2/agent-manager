75680a0dcf0b4537a6c7cf675e1805fb9245c02d
Anoshan Jayahanthan <101160077+AnoshanJ@users.noreply.github.com>
2026-03-09 11:08:20 +0530

    Refactor website to documentation directory

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -84,7 +84,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/wso2/agent-manager/edit/main/website/',
+            'https://github.com/wso2/agent-manager/edit/main/documentation/',
         },
         blog: false, // Disable blog until we have content
         theme: {
