cb3c0a72237717fc203740a04f245f37dfdbc071
hanzjk <hansi@wso2.com>
2026-03-26 18:39:10 +0530

    Fix PR check failures

--- agent-manager-service/clients/openchoreosvc/client/constants.go
+++ agent-manager-service/clients/openchoreosvc/client/constants.go
@@ -73,7 +73,7 @@ const (
 // -----------------------------------------------------------------------------
 
 const (
-	BuildEnvGoogleEntrypoint     = "GOOGLE_ENTRYPOINT"
+	BuildEnvGoogleEntrypoint = "GOOGLE_ENTRYPOINT"
 )
 
 // -----------------------------------------------------------------------------
