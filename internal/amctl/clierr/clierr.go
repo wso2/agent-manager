eb4c3d6f7a12fed976539105d40497b21e0cbdb3
Jhivan de Benoit <jhivanb@gmail.com>
2026-04-30 17:59:42 +0530

    Add Apache License 2.0 headers to amctl source files

--- internal/amctl/clierr/clierr.go
+++ internal/amctl/clierr/clierr.go
@@ -1,3 +1,19 @@
+// Copyright (c) 2026, WSO2 LLC. (https://www.wso2.com).
+//
+// WSO2 LLC. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 // Package clierr defines the CLI error wire type and stable error codes used
 // in the JSON envelope written by package render. CLIError doubles as a Go
 // error (so producers `return clierr.New(...)` and consumers extract via
