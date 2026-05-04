fe903b0b789e3bfa3756c4fb76a2fd48555b4336
Jhivan de Benoit <jhivanb@gmail.com>
2026-05-04 12:02:44 +0530

    Update oapi-codegen output paths for cli/ layout

--- cli/pkg/clients/amsvc/gen/doc.go
+++ cli/pkg/clients/amsvc/gen/doc.go
@@ -16,7 +16,7 @@
 
 // Package gen contains the auto-generated Agent Manager Service API client and types.
 //
-// Run `make am-gen-client` from the repository root to regenerate.
+// Run `make amctl-gen-client` from the repository root to regenerate.
 package gen
 
 //go:generate oapi-codegen -config oapi-codegen.yaml ../../../../../agent-manager-service/docs/api_v1_openapi.yaml
